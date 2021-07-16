ad151b71af32b34486adfe50a7f3d9c8a19f7afc
Justin Byrd <justin_byrd@hotmail.com>
2021-07-16 14:50:40 -0400

    added imports and fixed variables to fix errors in LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -30,13 +32,13 @@ const LoginForm = () => {
       // const response = await loginUser(userFormData);
 		const { data } = await login({ variables: { ...userFormData }});
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      console.log(data.user);
+      Auth.login(data.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
