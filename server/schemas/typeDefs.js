74d2bb5d07e2cc43863bbf490d7f59bb87660822
Justin Byrd <justin_byrd@hotmail.com>
2021-07-15 20:04:50 -0400

    added typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,15 @@ const typeDefs = gql`
 		password: String
 	}
 
+	type Book {
+		bookId: ID!
+		authors: [String]
+		description: String
+		title: String
+		image: String
+		link: String
+	}
+
 	type Auth {
 		token: String
 		user: User
@@ -16,9 +25,23 @@ const typeDefs = gql`
 	type Query {
 		me(userId: ID!): User
 		getAllUsers: [User]!
-		
 	}
 
+	input saveBookInput {
+		author: [String]
+		description: String
+		title: String
+		bookId: String
+		image: String
+		link: String
+	}
+
+	type Mutation {
+		removeBook(bookId: ID!): User
+		saveBook(bookInput: saveBookInput!): User
+		addUser(username: String!, email: String!, password: String!): Auth
+		login(email: String!, password: String!): Auth
+	}
 `;
 
 module.exports = typeDefs;
